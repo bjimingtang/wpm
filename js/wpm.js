3e8089b88d0f83fb3dcb4e063ade427e041e7597
bjimingtang <brendan.jiming.tang@gmail.com>
2021-07-26 16:41:04 -0700

    updateQuote function

--- js/wpm.js
+++ js/wpm.js
@@ -14,3 +14,22 @@ function updateTimer() {
     finishGame();
   }
 }
+
+function updateQuote() {
+  quote_text.textContent = null;
+  current_quote = "";
+
+  // put in 10 random words from the dictionary
+  for (let i = 0; i < 9; i++) {
+    current_quote = current_quote.concat(dictionary[Math.floor(Math.random() * dictionary.length)] + " ");
+  }
+  current_quote = current_quote.concat(dictionary[Math.floor(Math.random() * dictionary.length)]);
+
+  // separate each character and make an element
+  // out of each of them to individually style them
+  current_quote.split('').forEach(char => {
+    const charSpan = document.createElement('span')
+    charSpan.innerText = char
+    quote_text.appendChild(charSpan)
+  });
+}
