35dfb2bf3f3cb1a00d1aaa1c62a131bd60c8db47
bjimingtang <brendan.jiming.tang@gmail.com>
2021-07-26 16:42:02 -0700

    processCurrentQuote

--- js/wpm.js
+++ js/wpm.js
@@ -28,8 +28,64 @@ function updateQuote() {
   // separate each character and make an element
   // out of each of them to individually style them
   current_quote.split('').forEach(char => {
-    const charSpan = document.createElement('span')
-    charSpan.innerText = char
-    quote_text.appendChild(charSpan)
+    const charSpan = document.createElement('span');
+    charSpan.innerText = char;
+    quote_text.appendChild(charSpan);
   });
+}
+
+function processCurrentText() {
+
+  // get current input text and split it
+  curr_input = input_area.value;
+  curr_input_array = curr_input.split('');
+
+  // increment total characters typed
+  characterTyped++;
+
+  errors = 0;
+
+  quoteSpanArray = quote_text.querySelectorAll('span');
+  quoteSpanArray.forEach((char, index) => {
+    let typedChar = curr_input_array[index];
+
+    // character not currently typed
+    if (typedChar == null) {
+      char.classList.remove('correct_char');
+      char.classList.remove('incorrect_char');
+
+      // correct character
+    } else if (typedChar === char.innerText) {
+      char.classList.add('correct_char');
+      char.classList.remove('incorrect_char');
+
+      // incorrect character
+    } else {
+      char.classList.add('incorrect_char');
+      char.classList.remove('correct_char');
+
+      // increment number of errors
+      errors++;
+    }
+  });
+
+  // display the number of errors
+  error_text.textContent = total_errors + errors;
+
+  // update accuracy text
+  let correctCharacters = (characterTyped - (total_errors + errors));
+  let accuracyVal = ((correctCharacters / characterTyped) * 100);
+  accuracy_text.textContent = Math.round(accuracyVal);
+
+  // if current text is completely typed
+  // irrespective of errors
+  if (curr_input.length == current_quote.length) {
+    updateQuote();
+
+    // update total errors
+    total_errors += errors;
+
+    // clear the input area
+    input_area.value = "";
+  }
 }
